30b7da8db9c9124e2d4531877fa268e0e32848e8
Hiago Petris <hiago.petris@gmail.com>
2022-06-15 09:26:41 -0300

    Bearer

--- src/usuarios/index.js
+++ src/usuarios/index.js
@@ -2,5 +2,6 @@ module.exports = {
   rotas: require('./usuarios-rotas'),
   controlador: require('./usuarios-controlador'),
   modelo: require('./usuarios-modelo'),
-  estrategiasAutenticacao: require('./estategias-autenticacao')
+  estrategiasAutenticacao: require('./estategias-autenticacao'),
+  middlewaresAutenticacao: require('./middlewares-autenticacao')
 }
